e06c8ffaf4a0e9d1027007e5e93870dfbdcbf383
Kirsten <klouk001@ucr.edu>
2020-01-04 19:13:21 -0800

    Start of problem 3

---
+++ Euler3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+
+*/
+
+int main() {
+    std::uint64_t value = 600851475143;
+    
+}
